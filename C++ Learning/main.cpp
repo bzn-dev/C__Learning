f94d2bebbbc583fe9933167d465d91e27f6bdcd4
bzn-dev <rafaelbauer0406@gmail.com>
2024-02-29 10:54:16 -0300

    project started

---
+++ C++ Learning/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    string salve = "Salve";
+    cout << "Hello World!\n";
+    cout << salve;
+    return 0;
+}
